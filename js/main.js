0564b6f86c790a7046d0039aa9f5d01474e55257
Kerls2020 <Salah.saidmansy@gmail.com>
2024-10-02 09:17:15 +0300

    up

--- js/main.js
+++ js/main.js
@@ -1,3 +1,38 @@
+function change(s) {
+  console.log(s);
+  var img = document.getElementById("level_img");
+  img.setAttribute("src", s);
+}
+document.querySelectorAll(".smooth-scroll").forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    target.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+});
+// الهيدر
+fetch("../header.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("header").innerHTML = data;
+  });
+//الفوتر
+fetch("../footer.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("footer").innerHTML = data;
+  });
+
+//القائمة الجانبية
+fetch("../aside.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("asider").innerHTML = data;
+  });
+//فتح القائمة الجانبية
 function openSidebar() {
   var sidebar = document.getElementById("sidebar");
   if (sidebar.style.left === "-250px") {
@@ -6,18 +41,9 @@ function openSidebar() {
     sidebar.style.left = "-250px";
   }
 }
-function change(s) {
-  console.log(s);
-  var img = document.getElementById("level_img");
-  img.setAttribute("src", s);
-}
-document.querySelectorAll('.smooth-scroll').forEach(anchor => {
-  anchor.addEventListener('click', function(e) {
-      e.preventDefault();
-      const target = document.querySelector(this.getAttribute('href'));
-      target.scrollIntoView({
-          behavior: 'smooth',
-          block: 'start'
-      });
+//عرض الازرار الجانبية مثل الهوم و الاب
+fetch("../go_home.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("go_home").innerHTML = data;
   });
-});
